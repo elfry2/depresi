e0b443b76c94a509228865643dc775ea8f788992
Elfry <elfry3@gmail.com>
2023-05-16 09:36:09 +0700

    Update symptoms

--- database/seeders/RuleSeeder.php
+++ database/seeders/RuleSeeder.php
@@ -5,6 +5,83 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// [A B C D E] C 3
+// A B C
+// A B D
+// A B E
+// A C D
+// A C E
+// A D E
+// B C D
+// B C E
+// B D E
+// C D E
+// function factorial(float $number)
+// {
+//     if($number == 1) return 1;
+
+//     return factorial($number-1) * $number;
+// }
+
+// function generateCombination(
+//     array $data,
+//     int $size,
+//     array $mustInclude = null)
+// {
+//     // Calculate number of possible combinations
+//     $possibleCombinationCount
+//     = factorial(count($data))
+//     /(factorial($size)
+//     *factorial(count($data)-$size));
+    
+//     $combinations = [];
+    
+//     for($i = 0; $i < $possibleCombinationCount; $i++)
+//     {
+//     	$combination = array_slice($data, 0, $size);
+        
+//         if(in_array($combination, $combinations))
+//         {
+//         	for($j = $size-1; $j >= 0; $j--)
+//             {
+//             	$temp = $combination;
+                
+//                 $ok = false;
+
+//                 for($k = 0; $k < count($data); $k++)
+//                 {
+//                     if(!in_array($data[$k], $temp))
+//                     {
+//                         $temp[$j] = $data[$k];
+                        
+//                         if(!in_array($temp, $combinations))
+//                         {
+//                         	$ok = true;
+                        
+//                         	break;
+//                         }
+//                     }
+//                 }
+
+//                 if($ok)
+//                 {
+//                 	$combination = $temp;
+                    
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         array_push($combinations, $combination);
+//     }
+    
+//     return $combinations;
+// }
+
+// echo "<pre>";
+// print_r(generateCombination([A, B, C, D, E], 3));
+// echo "</pre>";
+
 class RuleSeeder extends Seeder
 {
     /**
