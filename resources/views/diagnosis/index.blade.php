5cea96023d8496b0bf93c024b8a3b1c3ca4eb889
Elfry <elfry3@gmail.com>
2023-06-12 20:16:47 +0700

    Update knowledge base

--- resources/views/diagnosis/index.blade.php
+++ resources/views/diagnosis/index.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.diagnosis')
 @section('content')
-<h2 class="text-center">Tenang saja.</h2>
-<p class="text-center mt-4">Masukan anda hanya disimpan selama sesi {{ strtolower($title) }} berlangsung dan dihapus secara permanen pada akhir sesi atau kapanpun anda menginginkannya. Kami tidak akan menanyakan informasi apapun yang dapat digunakan untuk mengidentifikasi anda.</p>
+<center>
+<h2>Tenang saja.</h2>
+<p class="mt-4">Masukan anda hanya disimpan selama sesi {{ strtolower($title) }} berlangsung dan dihapus secara permanen pada akhir sesi atau kapanpun anda menginginkannya. Kami tidak akan menanyakan informasi apapun yang dapat digunakan untuk mengidentifikasi anda.</p>
+<p>Lima atau lebih simtom berikut ini harus muncul selama periode dua minggu terakhir dan mengakibatkan perubahan dari fungsi sebelumnya; setidaknya simtom-simtom tersebut termasuk (1) suasana hati depresif, (2) kehilangan minat atau kesenangan.</p>
+</center>
 @endsection
 @section('actions')
 <a href="/{{ $resource }}/create" class="w-100"><button class="btn btn-outline-success rounded-0 border-0 w-100" style="min-height: 3em">Mulai sesi</button></a>
