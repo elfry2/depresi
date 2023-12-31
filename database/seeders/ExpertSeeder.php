6449815c7d8ad5247b8d2638aacc84166a0515e5
Elfry <elfry3@gmail.com>
2023-12-31 23:20:54 +0700

    First commit on public ver.

--- database/seeders/ExpertSeeder.php
+++ database/seeders/ExpertSeeder.php
@@ -15,21 +15,6 @@ class ExpertSeeder extends Seeder
      */
     public function run()
     {
-        foreach([
-            [
-                'name' => 'Gita Yolanda, M.Psi., Psikolog',
-                'address' => 'Jl. Sukarno Hatta Kompleks Damai Langgeng Blok B No. 22',
-                'phone_number' => 6285292856052,
-                'has_whatsapp' => 1,
-                'path_to_photo' => 'expert-photos/WhatsApp Image 2023-05-10 at 08.47.20.jpeg'
-            ],
-            [
-                'name' => 'Fara Ulfa, M.Psi., Psikolog',
-                'address' => 'Yayasan Praktik Psikolog Indonesia, Jl. Todak No. 18, Pekanbaru',
-                'phone_number' => 6281364286335,
-                'has_whatsapp' => 1,
-                'path_to_photo' => 'expert-photos/WhatsApp Image 2023-05-10 at 08.01.49 Cropped.jpg'
-            ]
-        ] as $expert) Expert::create($expert);
+        foreach([] as $expert) Expert::create($expert);
     }
 }
